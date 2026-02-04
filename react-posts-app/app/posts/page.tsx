ff6a90375f15f7f4ba91cbcf480e9d4476d34b17
TimVerbeeck <tim.lucario@gmail.com>
2026-02-04 15:28:34 +0100

    add page for the posts

---
+++ react-posts-app/app/posts/page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+    return(
+        <main>
+            <input id="postSearch" placeholder="Search post titles" />
+            <ul>
+                <li className="flex items-center gap-6">lorem ipsum</li>
+                <li className="flex items-center gap-6">lorem ipsum</li>
+                <li className="flex items-center gap-6">lorem ipsum</li>
+                <li className="flex items-center gap-6">lorem ipsum</li>
+                <li className="flex items-center gap-6">lorem ipsum</li>
+            </ul>
+        </main>
+    );
+}
