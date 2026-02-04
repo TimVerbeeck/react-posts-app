8d2da464af42337e2dcbadd5162902b25d432715
TimVerbeeck <tim.lucario@gmail.com>
2026-02-04 15:50:26 +0100

    add page for the post details

---
+++ react-posts-app/app/posts/[id]/page.tsx
@@ -0,0 +1,16 @@
+export default function Page() {
+    return(
+        <main>
+            <h1>Lorem Ipsum</h1>
+            <p>dolor si amet auia et suscipit sucipit recusandae consequuntur</p>
+            <h2>Comments</h2>
+            <ul>
+                <li>Lorem ipsum</li>
+                <li>Lorem ipsum</li>
+                <li>Lorem ipsum</li>
+                <li>Lorem ipsum</li>
+                <li>Lorem ipsum</li>
+            </ul>
+        </main>
+    );
+}
--- react-posts-app/app/posts/page.tsx
+++ react-posts-app/app/posts/page.tsx
@@ -1,13 +1,25 @@
+import Link from "next/dist/client/link";
+
 export default function Page() {
     return(
         <main>
             <input id="postSearch" placeholder="Search post titles" />
             <ul>
-                <li className="flex items-center gap-6">lorem ipsum</li>
-                <li className="flex items-center gap-6">lorem ipsum</li>
-                <li className="flex items-center gap-6">lorem ipsum</li>
-                <li className="flex items-center gap-6">lorem ipsum</li>
-                <li className="flex items-center gap-6">lorem ipsum</li>
+                <li className="flex items-center gap-6">
+                    <Link href={"/posts/1"}>lorem ipsum</Link>
+                </li>
+                <li className="flex items-center gap-6">
+                    <Link href={"/posts/2"}>lorem ipsum</Link>
+                </li>
+                <li className="flex items-center gap-6">
+                    <Link href={"/posts/3"}>lorem ipsum</Link>
+                </li>
+                <li className="flex items-center gap-6">
+                    <Link href={"/posts/4"}>lorem ipsum</Link>
+                </li>
+                <li className="flex items-center gap-6">
+                    <Link href={"/posts/5"}>lorem ipsum</Link>
+                </li>
             </ul>
         </main>
     );
