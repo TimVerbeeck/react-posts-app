d0a5c851fa7af3b7da5f64cc4f12cf42c455d58a
TimVerbeeck <tim.lucario@gmail.com>
2026-02-05 15:23:39 +0100

    add: - button that redirects to post detail page

--- react-posts-app/app/ui/posts-component.tsx
+++ react-posts-app/app/ui/posts-component.tsx
@@ -1,4 +1,5 @@
 import {fetchPosts} from "@/lib/data";
+import Link from "next/dist/client/link";
 
 export default async function PostsComponent() {
     const posts = await fetchPosts();
@@ -8,7 +9,10 @@ export default async function PostsComponent() {
             <input id="postSearch" placeholder="Search post titles"/>
             <ul>
                 {posts?.map(post => (
-                    <li key={post.id}>{post.title}</li>
+                    <li key={post.id}>
+                        <p>{post.title}</p>
+                        <Link key={post.id} className='bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded' href={`/posts/${post.id}`}>View</Link>
+                    </li>
                 ))}
             </ul>
         </main>
